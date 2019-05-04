ecf67974e64b4ee7ebbc4d3b44528a252de8dda9
Teodora <teodora_milia@yahoo.com>
2019-05-04 12:40:13 +0300

    Lab1

---
+++ Lab1/Models/ProductsDbSeeder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1.Models
+{
+    public class ProductsDbSeeder
+    {
+        public static void Initialize(ProductsDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any products.
+            if (context.Products.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Products.AddRange(
+                new Product
+                {
+                    Name = "Milka",
+                    Description = "Cookies",
+                    Category = "Sweets",
+                    Price = 2.00,
+                },
+                new Product
+                {
+                    Name = "Kinder Bueno",
+                    Description = "Chocolate",
+                    Category = "Sweets",
+                    Price = 2.00,
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
+
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Lab1.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Lab1
@@ -14,7 +16,24 @@ namespace Lab1
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ProductsDbContext>();
+                    ProductsDbSeeder.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
